c7484a4f2e8cb355b040e9fb466804eebd512a86
ANDERSON MELO DE ANDRADE <andersonevolution18@gmail.com>
2025-09-26 19:45:46 -0300

    checked

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-let nomes = ["Anderson", "João", "Claudio", "Thalia", "Carlos"];
+function calcularTotal(precoUnitario, quantidade) {
+    return precoUnitario * quantidade;
+}
 
-console.log("Terceiro nome:", nomes[2]);
+function aplicarDesconto(valorTotal) {
+    if (valorTotal > 100) {
+        return valorTotal * 0.90;
+    } else {
+        return valorTotal;
+    }
+}
 
-nomes.push("Thomas");
-nomes.unshift("Rafael");
-
-nomes.pop();
-console.log("Array atualizado:", nomes);
-
-const numeros = [2, 4, 6, 8];
-const numerosDobrados = numeros.map(num => num * 2);
-console.log("Números dobrados:", numerosDobrados);
-
-const numeros2 = [1, 3, 5, 7, 9];
-const maioresQueCinco = numeros2.filter(num => num > 5);
-console.log("Números maiores que 5:", maioresQueCinco);
